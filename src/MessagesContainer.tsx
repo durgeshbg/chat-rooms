5acbd1455f7612ba64964ac766afffe58561ee32
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-02-23 10:26:30 +0530

    update: to use chatbubble type

--- src/MessagesContainer.tsx
+++ src/MessagesContainer.tsx
@@ -1,8 +1,8 @@
 import { createRef, useEffect } from 'react';
-import { Message } from './types';
+import { ChatBubble } from './types/message';
 
 interface PropTypes {
-  messages: Message[];
+  messages: ChatBubble[];
 }
 
 const MessageContainer = ({ messages }: PropTypes) => {
@@ -13,18 +13,20 @@ const MessageContainer = ({ messages }: PropTypes) => {
   }, [messageContainerRef]);
 
   return (
-    <div ref={messageContainerRef} className='p-4 overflow-y-scroll border m-4 space-y-4'>
+    <div ref={messageContainerRef} className='p-4 overflow-y-scroll border m-4 h-[80vh] space-y-4'>
       {messages &&
-        messages.map((msg: Message, i: number) => (
-          <div className='flex items-center gap-3' key={i}>
-            <img
-              className='rounded-full size-8'
-              src={`https://ui-avatars.com/api/?name=${msg.payload.username}&background=bc0d33&color=fff`}
-              alt='Profile'
-            />
-            <div className='bg-white text-black w-max px-2 py-1 rounded-md'>{msg.payload.text}</div>
-          </div>
-        ))}
+        messages.map((message: ChatBubble, i: number) => {
+          return (
+            <div className={`flex items-center gap-3 w-full ${message.self ? 'flex-row-reverse' : ''}`} key={i}>
+              <img
+                className='rounded-full size-8'
+                src={`https://ui-avatars.com/api/?name=${message.username}&background=bc0d33&color=fff`}
+                alt='Profile'
+              />
+              <div className='px-2 py-1 text-black bg-white w-max rounded-md'>{message.text}</div>
+            </div>
+          );
+        })}
     </div>
   );
 };
