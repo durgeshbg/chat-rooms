2673db5627254a6343f97e28dbba0d7cd3ff382f
Durgesh <84910181+Durgesh-babu@users.noreply.github.com>
2025-02-23 10:27:58 +0530

    add: App logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,118 @@
+import { useEffect, useRef, useState } from 'react';
+import MessageContainer from './MessagesContainer';
+import TextBox from './TextBox';
+import { ChatBubble, ChatPayload, JoinExitPayload, Message } from './types/message';
+import SignUpForm from './SignUpForm';
+import createMessage from './utils/createMessage';
+import { toast, ToastContainer } from 'react-toastify';
+import { Log } from './types/log';
+
 function App() {
-  return <div className='text-3xl'>Hello</div>;
+  const [messages, setMessages] = useState<ChatBubble[]>([]);
+  const [roomId, setRoomId] = useState('1405ab34-7e8d-43d9-bcb4-3f1652ed216c');
+  const [username, setUsername] = useState('');
+  const [currentMessage, setCurrentMessage] = useState('');
+  const [insideRoom, setInsideRoom] = useState(false);
+  const wsRef = useRef<WebSocket | null>(null);
+
+  function sendMessage() {
+    const chatPayload: ChatPayload = {
+      username,
+      text: currentMessage,
+      room: roomId,
+    };
+    const message: Message = createMessage('chat', chatPayload);
+    wsRef.current?.send(JSON.stringify(message));
+    const chatBubble: ChatBubble = { ...chatPayload, self: true };
+    setMessages([...messages, chatBubble]);
+    setCurrentMessage('');
+  }
+
+  function joinRoom() {
+    const joinPayload: JoinExitPayload = {
+      username,
+      room: roomId,
+    };
+    const message: Message = createMessage('join', joinPayload);
+    wsRef.current?.send(JSON.stringify(message));
+    setCurrentMessage('');
+    setMessages([]);
+    setInsideRoom(true);
+  }
+
+  function exitRoom() {
+    const exitPayload: JoinExitPayload = {
+      username,
+      room: roomId,
+    };
+    const message: Message = createMessage('exit', exitPayload);
+    wsRef.current?.send(JSON.stringify(message));
+    setCurrentMessage('');
+    setMessages([]);
+    setRoomId('');
+    setUsername('');
+    setInsideRoom(false);
+  }
+
+  if (wsRef.current) {
+    wsRef.current.onmessage = (e: MessageEvent<string>) => {
+      const data = JSON.parse(e.data);
+      if (data.type === 'chat') {
+        const chatBubble: ChatBubble = { ...data.payload, self: false };
+        setMessages([...messages, chatBubble]);
+      } else {
+        const log: Log = data as unknown as Log;
+        if (log.payload.title === 'error') {
+          toast.error(JSON.stringify(log.payload.description));
+        } else if (log.payload.title === 'info') {
+          toast.info(JSON.stringify(log.payload.description));
+        } else if (log.payload.title === 'success') {
+          toast.success(JSON.stringify(log.payload.description));
+        } else if (log.payload.title === 'warn') {
+          toast.warn(JSON.stringify(log.payload.description));
+        }
+      }
+    };
+  }
+
+  useEffect(() => {
+    wsRef.current = new WebSocket('ws://localhost:8080');
+
+    return () => {
+      wsRef.current?.close();
+    };
+  }, []);
+
+  return (
+    <div className='flex flex-col py-4 text-white bg-slate-900 min-h-dvh'>
+      <div className='flex flex-col w-7/12 p-4 mx-auto border rounded-md'>
+        {!insideRoom ? (
+          <SignUpForm
+            username={username}
+            setUsername={setUsername}
+            roomId={roomId}
+            setRoomId={setRoomId}
+            joinRoom={joinRoom}
+          />
+        ) : (
+          <>
+            <div className='flex justify-between'>
+              <div className='text-xl text-center border-b-2 border-b-blue-700'>Room: {roomId}</div>{' '}
+              <button
+                onClick={exitRoom}
+                className='px-2 py-1 text-blue-700 border border-blue-700 hover:text-white hover:bg-blue-700 transition-colors rounded-md'
+              >
+                Exit Room
+              </button>
+            </div>
+            <MessageContainer messages={messages as ChatBubble[]} />
+            <TextBox currentMessage={currentMessage} setCurrentMessage={setCurrentMessage} sendMessage={sendMessage} />
+          </>
+        )}
+      </div>
+      <ToastContainer theme='dark' />
+    </div>
+  );
 }
 
 export default App;
--- src/SignUpForm.tsx
+++ src/SignUpForm.tsx
@@ -5,15 +5,16 @@ interface PropsType {
   setUsername: React.Dispatch<React.SetStateAction<string>>;
   roomId: string;
   setRoomId: React.Dispatch<React.SetStateAction<string>>;
+  joinRoom: () => void;
 }
 
-const SignUpForm = ({ username, setUsername, roomId, setRoomId }: PropsType) => {
+const SignUpForm = ({ username, setUsername, roomId, setRoomId, joinRoom }: PropsType) => {
   return (
-    <div className='flex flex-col gap-3 items-start'>
+    <div className='flex flex-col items-start gap-3'>
       <div className='flex items-center justify-between gap-4 w-96'>
         <label htmlFor='username'>Username:</label>
         <input
-          className='px-3 py-1 border border-gray-300 rounded-md w-72 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black'
+          className='px-3 py-1 text-black border border-gray-300 rounded-md w-72 focus:outline-none focus:ring-2 focus:ring-blue-500'
           id='username'
           type='text'
           value={username}
@@ -23,7 +24,7 @@ const SignUpForm = ({ username, setUsername, roomId, setRoomId }: PropsType) => 
       <div className='flex items-center justify-between gap-4 w-96'>
         <label htmlFor='room'>Room ID:</label>
         <input
-          className='px-3 py-1 border border-gray-300 rounded-md w-72 focus:outline-none focus:ring-2 focus:ring-blue-500 text-black'
+          className='px-3 py-1 text-black border border-gray-300 rounded-md w-72 focus:outline-none focus:ring-2 focus:ring-blue-500'
           id='room'
           type='text'
           value={roomId}
@@ -35,11 +36,14 @@ const SignUpForm = ({ username, setUsername, roomId, setRoomId }: PropsType) => 
         onClick={() => {
           setRoomId(uuid.v4());
         }}
-        className='px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-200'
+        className='px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-200'
       >
         Generate room ID
       </button>
-      <button className='px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition duration-200'>
+      <button
+        onClick={joinRoom}
+        className='px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition duration-200'
+      >
         Join Room
       </button>
     </div>
